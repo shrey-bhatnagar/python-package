a74a29af9e423d467b2a1a7be21bb2ece48cd3d1
shrey <Shrey.Bhatnagar@infinite.com>
2017-11-30 15:01:02 +0530

    Added nosetests

--- new.py
+++ new.py
@@ -6,22 +6,30 @@
 #from mainShrey.cc import c
 #from mainShrey.dd import d
 import mainpackage
-from mainpackage import main_func
+#from mainpackage import main_func
 def new():
     print('\r\n we are in file-new.py \r\n')
     return 10
 
+"""
 print(new())
 print(main_func())
 print(mainpackage.a())
 print(mainpackage.b())
 print(mainpackage.c())
 print(mainpackage.d())
+"""
 
 def shrey_assert(val):
-    assert (val >= 0),"val is less than ZERO!"
-    return val**val
+    if(val >= 1):
+        return val
+    elif(val == 0):
+        return val
+    else:
+        return None
+    #assert (val >= 0),"val is less than ZERO!"
 
-shrey_assert(1)### negative value to raise assert
+#shrey_assert(1)### negative value to raise assert
 
 
+print('\r\nexecution of new.py completed\r\n')
---
+++ test_new.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from new import shrey_assert
+
+
+def test_1():
+    assert shrey_assert(1) == 1
+
+def test_2():
+    assert shrey_assert(0) == 0
+
+def test_3():
+    assert shrey_assert(-1) == None
